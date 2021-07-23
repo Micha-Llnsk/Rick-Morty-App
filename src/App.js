92645e456297462bf0b391bb341eb4e153db7ac9
Michael Lulinski <michael.lulinski@gmx.de>
2021-07-23 11:48:01 +0200

    Add links to static pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Switch,
+  Route,
+} from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import AllCharacters from "./pages/AllCharacters";
+import OneCharacter from "./pages/OneCharacter";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <nav className="App__nav">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/allcharacters">Characters</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/onecharacter">
+              <OneCharacter />
+            </Route>
+            <Route path="/allcharacters">
+              <AllCharacters />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="*">
+              <h2>No match </h2>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/OneCharacter.js
@@ -0,0 +1,10 @@
+export default function OneCharacter() {
+  return (
+    <section>
+      <h2>title</h2>
+      <img src="" alt="Rick Sanchez" />
+      <p>Status</p>
+      <p>Gender</p>
+    </section>
+  );
+}
