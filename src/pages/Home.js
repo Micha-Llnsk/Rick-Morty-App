76d906fa6ab131ea9686ccd1d6c8c612c4b26b25
Micha-Llnsk <michael.lulinski@gmx.de>
2022-07-27 15:36:02 +0200

    Add dynamic char-card background for status

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,13 @@ import "./Home.css";
 export default function Home() {
   return (
     <section className="App__homeContent">
-      <h1>Welcome to the Rick and Morty App = )</h1>
+      <h1>Rick and Morty App</h1>
       <img
         className="App__homeImage"
         src={portal}
         alt="Rick and Morty fighting their out of a portal"
       />
-      <p>Done with love by Micha</p>
+      <p>Done by Micha</p>
     </section>
   );
 }
