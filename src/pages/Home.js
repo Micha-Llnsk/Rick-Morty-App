a59c4509509ae018031ee7f631fcc96b650d0dd4
Micha-Llnsk <michael.lulinski@gmx.de>
2022-07-28 04:55:09 +0200

    Add github link, Change text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,20 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <section className="App__homeContent">
-      <h1>Rick and Morty App</h1>
-      <img
-        className="App__homeImage"
-        src={portal}
-        alt="Rick and Morty fighting their out of a portal"
-      />
-      <p>Done by Micha</p>
+    <section className="Home__Content">
+      <div className="Home__Text">
+        <h2>Welcome</h2>
+        <p>to the Rick and Morty</p>
+        <h2>Multiverse !!</h2>
+      </div>
+        <img
+          className="Home__Image"
+          src={portal}
+          alt="Rick and Morty fighting their way out of a portal"
+        />
+      <div className="Home__Sign">
+        <a className="Home__Sign--Link" href="https://github.com/Micha-Llnsk" target="_blank" rel="noreferrer">Made by Micha-Llnsk</a>
+      </div>
     </section>
   );
 }
