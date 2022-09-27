5cd2137646a2a2aa9cc4406c44005c1c67fbc594
Micha-Llnsk <michael.lulinski@gmx.de>
2022-09-27 22:25:09 +0200

    Add confrim for delete

--- src/pages/FavCharacters.js
+++ src/pages/FavCharacters.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { ReactComponent as FavYellow } from "../images/star_yellow.svg";
+import { ReactComponent as FavBlack } from "../images/star_black.svg";
 
 
 export default function FavCharacters() {
@@ -18,29 +18,36 @@ export default function FavCharacters() {
 
     function renderCharacters() {
         const ListOfChars = favChars.map((char) => {
-          let statusClass;
-          switch (char.status) {
-            case "Alive":
-              statusClass = "CharCard--alive";
-              break;
-              case "Dead":
-                statusClass = "CharCard--dead";
-                break;
-                default:
-                  statusClass = "CharCard--unknown";
-                  break;
-                }
+            let statusClass;
+            switch (char.status) {
+                case "Alive":
+                    statusClass = "CharCard--alive";
+                    break;
+                    case "Dead":
+                        statusClass = "CharCard--dead";
+                        break;
+                        default:
+                            statusClass = "CharCard--unknown";
+                            break;
+                        }
+                        
+    function handleDeleteFavorite() {
+        if (window.confirm("Do you want to delete this Char from your Favorites?")) {
+            localStorage.removeItem(char.id)
+            }
+    }
+
                 return (
-                    <li className={`List__Item--Wrap ${statusClass}`} key={char.id}>
-                      <Link className="List__Item" to={`/character/${char.id}`}>
-                        <img src={char.image} alt={char.name} />
-                        <div className="List__Item--Text">
-                          <h2>{char.name}</h2>
-                          <p>Status: {char.status}</p>
-                          <p>Gender: {char.gender}</p>
-                          <FavYellow className="char__icon" />
-                        </div>
-                      </Link>
+                    <li className={`List__Item--Wrap flex ${statusClass}`} key={char.id}>
+                        <Link className="List__Item" to={`/character/${char.id}`}>
+                            <img src={char.image} alt={char.name} />
+                            <div className="List__Item--Text">
+                                <h2>{char.name}</h2>
+                                <p>Status: {char.status}</p>
+                                <p>Gender: {char.gender}</p>
+                            </div>
+                        </Link>
+                        <FavBlack className="char__icon active" onClick={handleDeleteFavorite} />
                     </li>
                   );
                 });
